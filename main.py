6c43520ed707b7d95377c097baa55b6a7ada2c66
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-11-02 23:24:49 +0600

    ADD: main.py and index.html

---
+++ main.py
@@ -0,0 +1,35 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    @staticmethod
+    def __get_html_file():
+        with open('index.html', encoding='utf-8') as index:
+            return index.read()
+
+
+    def do_GET(self):
+        parse_qs(urlparse(self.path).query)
+        page_content = self.__get_html_file()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
